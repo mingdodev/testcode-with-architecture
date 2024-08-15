d1e4f220b931dec79e91bd84b602491be6301155
mingdodev <mingdo.dev@gmail.com>
2024-08-15 15:07:50 +0900

    UserService read 메소드 테스트

---
+++ src/test/java/com/example/demo/service/UserServiceTest.java
@@ -0,0 +1,72 @@
+package com.example.demo.service;
+
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.UserEntity;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+@TestPropertySource("classpath:test-application.properties")
+@SqlGroup({
+    @Sql(value = "/sql/user-service-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+    @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void getByEmail은_ACTIVE_상태인_유저를_찾아올_수_있다() {
+        // given
+        String email = "user1@naver.com";
+
+        // when
+        UserEntity result = userService.getByEmail(email);
+
+        // then
+        Assertions.assertThat(result.getNickname()).isEqualTo("user1");
+    }
+
+    @Test
+    void getByEmail은_PENDING_상태인_유저를_찾아올_수_없다() {
+        // given
+        String email = "user2@naver.com";
+
+        // when
+        // then
+        // 해당 람다식이 호출되면 이 에러가 발생한다.
+        assertThatThrownBy(() -> {
+            UserEntity result = userService.getByEmail(email);
+        }).isInstanceOf(ResourceNotFoundException.class);
+    }
+
+    @Test
+    void getById은_ACTIVE_상태인_유저를_찾아올_수_있다() {
+        // given
+        // when
+        UserEntity result = userService.getById(1);
+
+        // then
+        Assertions.assertThat(result.getNickname()).isEqualTo("user1");
+    }
+
+    @Test
+    void getById은_PENDING_상태인_유저를_찾아올_수_없다() {
+        // given
+        // when
+        // then
+        // 해당 람다식이 호출되면 이 에러가 발생한다.
+        assertThatThrownBy(() -> {
+            UserEntity result = userService.getById(2);
+        }).isInstanceOf(ResourceNotFoundException.class);
+    }
+
+}
